1753d8c9902461be9baf2cf911dc2ed287413a8d
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2017-03-24 18:48:26 -0300

    Added comments to cep.c

--- cep.c
+++ cep.c
@@ -1,4 +1,4 @@
-#include "cep.h"    
+#include "cep.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -11,11 +11,23 @@ typedef struct {
     char[CEP_DESC_SIZE] description;
 }cep_t;
 
-bool compare_cep_by_num(cep_num_t *num, cep_t *cep) 
+/*
+* Compare two cep numbers
+* @param cep_num_t *num Number to be compare to a cep
+* @param cep_t *cep A cep to be compare
+* @return Return any number case it's true and 0 in case it's false
+*/
+bool compare_cep_by_num(cep_num_t *num, cep_t *cep)
 {
     return *num == cep->num;
 }
 
+/*
+* Compare tow descriptions
+* @param char[] description The description that you want to compare
+* @param cep_t *cep The cep you want to be compare
+* @return Return any number case it's true and 0 in case it's false
+*/
 bool compare_cep_by_desc(char[] description, cep_t *cep)
 {
     return str_cmp(description, cep->description) == 0;
